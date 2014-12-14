76666a0b79c54713423a4d1a8c50855912b2bf8a
Alex Carrega <contact@alexcarrega.com>
2014-12-14 07:19:49 +0100

    Major various changes

--- gulpfile.js
+++ gulpfile.js
@@ -11,43 +11,29 @@ var __join = axc_gulp.plugin.path.join;
 var assets_directory = 'assets';
 var bower_directory = 'vendor';
 
-function __bower(path)
-{
-	return __join(bower_directory, path);
-}
+function __bower(path) { return __join(bower_directory, path); }
+function __assets(path) { return __join(assets_directory, path); }
 
 axc_gulp.config.set({
-	name: 'theme',
 	title: 'Seldac Theme',
 	path: __join(__dirname, assets_directory),
 	icon_notify: __join(__dirname, assets_directory, 'images/gulp/icon/notify.png')
 });
 
 axc_gulp.task
-	.js2dist({ taskName: 'js2dist-bootstrap', src: __bower('bootstrap/js/**') })
-	.js2dist({ taskName: 'js2dist-bootstrap-checkbox', src: __bower('bootstrap-checkbox/js/**') })
-	.js2dist({ taskName: 'js2dist-jasny-bootstrap', src: __bower('jasny-bootstrap/js/**') })
+	.clean({ taskName: 'bootstrap-clean', src: __bower('bootstrap/less/variables.less') })
+	.copy({ taskName: 'bootstrap-font-copy', src: __bower('bootstrap/fonts/**/*.{ttf,woff,eot,svg}'), dest: 'fonts/bootstrap' })
+	.less2css({ taskName: 'bootstrap', src: __bower('bootstrap/less/bootstrap.less'), dependences: ['bootstrap-clean', 'bootstrap-font-copy'],
+		paths: [ __assets('less/bootstrap') ], dest: 'dist' })
 
-	.clean({ taskName: 'clean-bootstrap', src: __bower('bootstrap/less/variables.less') })
-	.less2css({ taskName: 'less2css-bootstrap', src: __bower('bootstrap/less/bootstrap.less'), dependences: ['clean-bootstrap'] })
-	.css2dist({ taskName: 'css2dist-bootstrap', src: __bower('bootstrap/less/bootstrap.less'), dependences: ['less2dist-bootstrap'] })
-	.copy({ taskName: 'copy-bootstrap-font', src: __bower('bootstrap/fonts/**'), dest: 'fonts/bootstrap' })
+	.clean({ taskName: 'jasny-bootstrap-clean', src: __bower('jasny-bootstrap/less/variables.less') })
+	.less2css({ taskName: 'jasny-bootstrap', src: __bower('jasny-bootstrap/less/jasny-bootstrap.less'), dependences: ['jasny-bootstrap-clean'],
+		paths: [ __bower('bootstrap/less'), __assets('less/jasny-bootstrap') ], dest: 'dist' })
 
-	.clean({ taskName: 'clean-jasny-bootstrap', src: __bower('jasny-bootstrap/less/variables.less') })
-	.less2css({ taskName: 'less2css-jasny-bootstrap', src: __bower('jasny-bootstrap/less/bootstrap.less'), dependences: ['clean-jasny-bootstrap'], paths: [
-		__bower('bootstrap/less',
-		__bower('jasny-bootstrap/less',
-		assets_directory + 'less/jasny-bootstrap'
-	] })
-	.css2dist({ taskName: 'css2dist-jasny-bootstrap', src: __bower('jasny-bootstrap/less/bootstrap.less'), dependences: ['clean-jasny-bootstrap'], paths: [
+	.js2dist({ taskName: 'theme-js', src: 'js/theme.js' })
+	.less2css({ taskName: 'theme-less', src: 'less/theme.less', paths: [ __bower('bootstrap/less'), assets_directory ], dest: 'dist' })
+	.run({ 'taskName': 'theme', dependences: ['theme-js', 'theme-less'] })
 
-	.clean({ taskName: 'clean-font-awesome', src: __bower('fontawesome/less/variables.less') })
-	.less({ taskName: 'less-font-awesome', src: __bower('fontawesome/less/font-awesome'), dependences: ['clean-font-awesome'], paths: [
-		__bower('font-awesome/less',
-		assets_directory + 'less/font-awesome'
-	] })
-	.copy({ taskName: 'copy-fontawesome-font', src: __bower('fontawesome/fonts/**'), dest: 'fonts/font-awesome' });
+	.clean({ src: ['dist/**', 'css/**', 'fonts/**'] })
 
-	less2css({ taskName: 'css-theme', src: 'less/theme.less' })
-	css2dist({ taskName: 'theme-css2dist', src: 'less/theme.less' })
-*/
+	.default(['bootstrap', 'jasny-bootstrap', 'theme']);
